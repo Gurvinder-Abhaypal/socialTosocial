6b090f34fd7a1c87d9862acf1984e5a658a9e88f
Gurvinder Singh <gurvindersingh@Gurvinders-MacBook-Air.local>
2019-09-19 19:38:47 +0530

    Checkin for the results from custom search engine

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { GetUrlStatusService } from './services/get-url-status.service';
+import { SitesList } from './Models/social-sites-list';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,24 @@ import { GetUrlStatusService } from './services/get-url-status.service';
 export class AppComponent {
   title = 'demo-app';
   urlPath: string;
+  result: any;
+  sitesList = new SitesList();
+  input = '';
+  // @ViewChild('inputBar') inputBar: any;
 
   constructor(private getUrlStatusService: GetUrlStatusService) {
 
   }
 
-  inputEntered($event: any) {
-    this.urlPath = $event.target.value;
-    this.getUrlStatusService.getUrlStatus(this.urlPath).subscribe(res => {
-      console.log(res.status);
+  onKey(event: any) {
+    this.input += event.target.value;
+  }
+
+  onSubmit() {
+    this.getUrlStatusService.getUrlStatus(this.sitesList.googleapilink + '&q=' + this.input).subscribe(res => {
+      this.result = res.body.items;
+    },() => {}, () => {
+      this.input = '';
     });
   }
 }
