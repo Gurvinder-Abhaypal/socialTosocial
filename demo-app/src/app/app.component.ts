d16bf2e62f014b1d8013dc1101fea350b8765aa7
Gurvinder Singh <gurvindersingh@Gurvinders-MacBook-Air.local>
2019-09-25 21:13:06 +0530

    made the changes for parsing the url and searching the rest  of the query params

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -29,21 +29,36 @@ export class AppComponent {
       this.resetSearch();
     }
   }
+  getHostname = (): string => {
+    const match = this.input.match(/(https?:\/\/)?(www[0-9]?\.)?(.[^/:]+\/)/i);
+    if (match != null && match.length > 2 && typeof match[0] === 'string' && match[0].length > 0) {
+      return match[0];
+    } else {
+      return null;
+    }
+
+  }
 
   loadData() {
     if (this.prevValue !== undefined && this.result.length > 0 && this.prevValue !== this.input) {
       this.resetSearch();
     }
+
+    const hostName = this.getHostname();
+    console.log('hostName: ', hostName);
+    const searchInp = hostName !== null ? this.input.split(hostName)[1] : this.input;
+    console.log(searchInp);
+
+
     if (this.input.length > 0) {
       // tslint:disable-next-line: max-line-length
-      this.getUrlStatusService.getUrlStatus(this.sitesList.googleapilink + '&q=' + this.input + '&start=' + this.startIndex).subscribe(res => {
+      this.getUrlStatusService.getUrlStatus(this.sitesList.googleapilink + '&q=' + searchInp + '&start=' + this.startIndex).subscribe(res => {
         if (res.body.items !== undefined) {
           res.body.items.forEach(element => {
             this.result.push(element.link);
           });
-          // this.result += res.body.items;
-          this.startIndex = res.body.queries.nextPage[0].startIndex;
-          if (+res.body.searchInformation.totalResults > 10) {
+          this.startIndex = res.body.queries.nextPage !== undefined ? res.body.queries.nextPage[0].startIndex : 1;
+          if (res.body.queries.nextPage !== undefined && +res.body.searchInformation.totalResults > 10) {
             this.showMore = true;
           }
         } else {
